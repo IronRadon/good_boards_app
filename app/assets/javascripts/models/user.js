7a3c1389f5ca93b179742a8b968e1f7e1d720d50
Fern Wood <wood.fern.l@gmail.com>
2014-01-18 01:07:48 -0800

    refactor user show and embedded view

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,7 +1,7 @@
 GoodBoardsApp.Models.User = Backbone.Model.extend({
-	initialize: function(models, options) {
-		this.user_id = options.user_id
-	},
+	// initialize: function(models, options) {
+	// 	this.user_id = options.user_id
+	// },
 
 	parse: function (data) {
 	  var reviews = data.reviews;
@@ -12,6 +12,11 @@ GoodBoardsApp.Models.User = Backbone.Model.extend({
 	},
 
 	url: function(){
-		return 'api/users/' + this.user_id
+		if (this.id){
+			return 'api/users/' + this.id;	
+		} else {
+			return 'api/users';
+		}
+		
 	}
 })
--- app/assets/javascripts/views/boardgames/boardgame_show.js
+++ app/assets/javascripts/views/boardgames/boardgame_show.js
@@ -22,7 +22,7 @@ GoodBoardsApp.Views.BoardgameShow = Backbone.View.extend({
 		this.model.get('reviews').each(function(review){
 			var user_id = review.get('user_id');
 			var id = review.get('id');
-			var user = new GoodBoardsApp.Models.User([], {user_id: user_id});
+			var user = new GoodBoardsApp.Models.User({id: user_id});
 			user.fetch({
 				success: function() {
 					var view = new GoodBoardsApp.Views.ReviewEmbedShowBoard({
